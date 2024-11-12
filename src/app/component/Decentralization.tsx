c5b8c97e6716934216fc30bc1be36f4d737b087e
umersheikh1986 <142933742+umersheikh1986@users.noreply.github.com>
2024-11-12 20:36:44 +0500

    security section upadated

--- src/app/component/Decentralization.tsx
+++ src/app/component/Decentralization.tsx
@@ -6,16 +6,20 @@ const Decentralization = () => {
       <div className="mt-8">
         <h1 className="text-center text-7xl text-white"> Security</h1>
       </div>
-      <section className="grid lg:grid-cols-2 gap-10 max-w-7xl items-center mx-auto  p-5">
-        <div className="p-8 border-2 border-white  rounded-[40px]">
+      <section className=" gap-10 max-w-7xl  mx-auto  p-5">
+        <div className="p-8 border-2 flex flex-col items-center justify-between text-white border-white  rounded-[40px]">
           {/* <Image src="/brands.png" width={400} height={400} alt="brands" /> */}
-          <Image src="/s1.png" width={400} height={400} alt="brands" />
 
-          <div className="text-white">
+          <div className="">
+            <Image src="/s1.png" width={400} height={400} alt="brands" />
+          </div>
+          <div>
             <h1 className="text-2xl md:text-3xl font-extrabold pt-8">
               Advanced Security Measures
             </h1>
-            <p className="mt-2">
+          </div>
+          <div>
+            <p className="mt-2 text-center">
               The Blockchain technology is considered secure because of its use
               of encryption to manage data, decentralization, and consensus.
               Gentop is a token create through a verified smart contract in the
@@ -24,32 +28,6 @@ const Decentralization = () => {
               were used building its infrastructure, guaranteeing compliance and
               transparency.
             </p>
-          </div>
-        </div>
-        <div className="text-white gap-16 ">
-          <div className="grid place-items-center rounded-[40px] justify-between border-2 border-white grid-cols-2 gap-4 p-2">
-            <h2 className="text-xl md:text-2xl  p-8">
-              Lorem Ipsum is simply a text
-            </h2>
-            <Image
-              src="/s2.png"
-              alt="arrow"
-              width={200}
-              height={200}
-              className="p-5 "
-            />
-          </div>
-          <div className="grid grid-cols-2 place-items-center rounded-[40px] justify-between border-2 border-white gap-4">
-            <h2 className="text-xl md:text-2xl  p-8">
-              Lorem Ipsum is simply a text
-            </h2>
-            <Image
-              src="/s3.png"
-              alt="arrow"
-              width={200}
-              height={200}
-              className="pt-5  "
-            />
           </div>
         </div>
       </section>
