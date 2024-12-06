42e6c465bb052173c80360c95b576901939a2ae1
umersheikh1986 <142933742+umersheikh1986@users.noreply.github.com>
2024-12-06 18:33:30 +0500

    changes done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import HomeHeader from "./component/HomeHeader";
 import HeroSection from "./component/HeroSection";
@@ -8,18 +9,21 @@ import Decentralization from "./component/Decentralization";
 import CombinedLevel from "./component/combinedlevel";
 import Registration from "./component/registration";
 import IndividualLevel from "./component/individuallevel";
-
+import { WalletProvider } from "./coonectWallet";
 export default function Home() {
   return (
     <main>
       {/* <HomePageHeader /> */}
-      <HeroSection />
-      <Solidarity />
-      <Mentality />
-      <Decentralization />
-      {/* <CombinedLevel /> */}
-      <IndividualLevel />
-      {/* <Registration /> */}
+      <WalletProvider>
+        <HomeHeader />
+        <HeroSection />
+        <Solidarity />
+        <Mentality />
+        <Decentralization />
+        {/* <CombinedLevel /> */}
+        <IndividualLevel />
+        {/* <Registration /> */}
+      </WalletProvider>
     </main>
   );
 }
