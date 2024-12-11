276362b13f1c4ce0dbdadde9ca0f30c470c78961
umersheikh1986 <142933742+umersheikh1986@users.noreply.github.com>
2024-12-11 20:53:14 +0500

    chnages made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,26 +4,22 @@ import HomeHeader from "./component/HomeHeader";
 import HeroSection from "./component/HeroSection";
 import Solidarity from "./component/Solidarity";
 import Mentality from "./component/Mentality";
-import HomePageHeader from "./component/HomePageheader";
 import Decentralization from "./component/Decentralization";
 import CombinedLevel from "./component/combinedlevel";
 import Registration from "./component/registration";
 import IndividualLevel from "./component/individuallevel";
-import { WalletProvider } from "./coonectWallet";
+
 export default function Home() {
   return (
     <main>
-      {/* <HomePageHeader /> */}
-      <WalletProvider>
-        <HomeHeader />
-        <HeroSection />
-        <Solidarity />
-        <Mentality />
-        <Decentralization />
-        {/* <CombinedLevel /> */}
-        <IndividualLevel />
-        {/* <Registration /> */}
-      </WalletProvider>
+      {/* <HomeHeader /> */}
+      <HeroSection />
+      <Solidarity />
+      <Mentality />
+      <Decentralization />
+      {/* <CombinedLevel /> */}
+      <IndividualLevel />
+      {/* <Registration /> */}
     </main>
   );
 }
