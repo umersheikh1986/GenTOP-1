ef36b4d8494c26640ae581be26e7b9512e3f1122
umersheikh1986 <142933742+umersheikh1986@users.noreply.github.com>
2025-02-27 18:18:59 +0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -64,17 +64,12 @@ const geistMono = localFont({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        {/* <WalletProvider>
+      <body className="bg-[#14000b]">
+        <WalletProvider>
           <HomeHeader />
           {children}
           <Registration />
-        </WalletProvider> */}
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
-          <h1 className="text-3xl md:text-5xl font-bold text-gray-800">
-            WEBSITE IS UNDER CONSTRUCTION
-          </h1>
-        </div>
+        </WalletProvider>
       </body>
     </html>
   );
