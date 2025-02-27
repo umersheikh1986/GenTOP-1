85b1ed5bf8299c5b78235b8648152a9f729f3d24
umersheikh1986 <142933742+umersheikh1986@users.noreply.github.com>
2025-02-27 16:32:14 +0500

    code pushed

--- src/app/component/UserDashboard1.js
+++ src/app/component/UserDashboard1.js
@@ -18,7 +18,7 @@ const UserDashboard1 = () => {
     useContext(WalletContext);
   const [loading, setLoading] = useState(false);
 
-  const [totalPurchasedToken, settotalPurchasedToken] = useState("5");
+  const [totalPurchasedToken, settotalPurchasedToken] = useState(null);
   const [wa, setWA] = useState(walletAddress);
 
   const [No_Of_Purchases, setNo_Of_Purchases] = useState([]);
